deb6a3ce65b00a7323581ffd71004b59c5852106
170740874LH <112689518+170740874LH@users.noreply.github.com>
2023-06-07 18:21:44 +0100

    Add files via upload

---
+++ healthBar.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+
+public class healthBar {
+    private int x,y, height,health;
+    private Color colour;
+
+    public healthBar (int x, int y,int height,Color colour, int life) {
+        this.x = x;
+        this.y = y;
+        this.height = height;
+        this.colour = colour;
+        this.health = life;
+    }
+
+    public void paint (Graphics g) {
+        g.setColor(colour);
+        g.fillRect(x, y, health, height);
+        
+    }
+    public void updateHealth (int hearts) {
+        System.out.println(health);
+        this.health = hearts;
+    }
+}
